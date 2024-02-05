2be937fcc8f32b1c4c6e831612d1bf81f660959a
Vignesh <vignesh@onelabventures.com>
2024-02-05 20:29:51 +0530

    modified Homepage

--- src/taskpane/components/Home.jsx
+++ src/taskpane/components/Home.jsx
@@ -1,83 +1,81 @@
-
 import React, { useState } from 'react';
 import "./styles/Home.css";
-import logo1 from "../../../public/w2.png";
-import logo2 from "../../../public/w3.png";
-import logo3 from "../../../public/w4.png";
-import cross from "../../../public/Vector.png";
-
-// import { Button } from '@material-ui/core';
-
 
 
 const Home = ({ emailDetails, ...rest  }) => {
 
-    const [fontSize, setFontSize] = useState(14); // Default font size
+  
+  const [fontSize, setFontSize] = useState(10); // Default font size
 
-    const increaseFontSize = () => {
-        if (fontSize < 20) setFontSize(fontSize + 1);
-    };
+  
 
-    const decreaseFontSize = () => {
-        if (fontSize > 14) setFontSize(fontSize - 1);
-    };
+  const increaseFontSize = () => {
+    if (fontSize < 20) setFontSize(fontSize + 1);
+  };
+  const decreaseFontSize = () => {
+    if (fontSize > 3) setFontSize(fontSize - 1);
+  };
 
   const DelegateBtn = () => {
-    alert("Delegate Button is clicked");
+    console.log('del');
+    // navigate("/del");
   };
 
   const queueBtn = () => {
-    alert("Queue Button is clicked");
+    console.log('del');
+    // navigate("/queue");
   };
 
-  const conatct = () => {
-    alert("Contact Us");
-  };
 
   return (
     <div className="homepage">
-      {/* Section -1 */}
+      <div className="topbar-frame">
+        {/* Section -1 */}
 
-      {/* <div className="topbar">
-        <div className="topframe">
-          <img src={logo1} alt="Logo" />
-          <div className="topframe-b">
-            <img src={logo2} alt="Logo" id="b1" />
-            <img src={logo3} alt="Logo" id="b2" />
-          </div>
-        </div>
-        <div className="topframe-c">
-          <img src={cross} alt="Logo" />
-        </div>
-      </div> */}
-
-      {/* Section - 2 */}
-      <div className="topbar2">
-        <div className="a2">
-          <div className="home">
-            <div id="home-div">Home</div>
-          </div>
-          <div className="contact">
-            <div onClick={conatct} id="contact-div">
-              Contact Us
+        {/* Commented Topbar */}
+        
+        {/* <div className="topbar">
+          <div className="topframe">
+            <img src={W_Image} alt="Logo" />
+            <div className="topframe-b">
+              <img src={WiseImage} alt="Logo" id="b1" />
+              <img src={InsideImage} alt="Logo" id="b2" />
             </div>
           </div>
-        </div>
+          <div onClick={handleCrossClick} className="topframe-c">
+            <img src={cross} alt="Logo" />
+          </div>
+        </div> */}
 
-        <div className="A">
-          <div id="A-child">A</div>
+        {/* Section - 2 */}
+
+        <div className="topbar2">
+          <div className="navbar">
+            <div className="delegate-div">
+              <div className="delegate">Delegate</div>
+            </div>
+            <div className="Homequeue-div">
+              <div className="queue">Queue</div>
+            </div>
+            <div className="contact-div">
+              <div className="contact">Contact Us</div>
+            </div>
+          </div>
+          <div className="A-div">
+            <div className="A">A</div>
+          </div>
         </div>
       </div>
 
       {/* Section - 3 */}
 
-      <div className="frame">
-        <div className="main-frame">
-          <div className="table-header">
+      <div className="home-middleFrame">
+        <div className="homeMainframe">
+          <div className="homeTable-header">
             <div className="datagridHeader">
-              <div className="container">
-                <div className="typography">
-                  <div className="typography-text">RFQ</div>
+              <div className="homeContainer">
+                <div className="homeTypography">
+                  <div className="home-RFQ">RFQ</div>
                 </div>
                 <div className="FontSize">
                   <div className="sizeDecrement">
@@ -87,7 +85,7 @@ const Home = ({ emailDetails, ...rest  }) => {
                   </div>
                   <div className="sizePercentage">
                     <div className="Percentage">
-                    {Math.round((fontSize / 10) * 100)}%
+                      {Math.round((fontSize / 10) * 100)}%
                     </div>
                   </div>
                   <div className="sizeIncrement">
@@ -95,34 +93,20 @@ const Home = ({ emailDetails, ...rest  }) => {
                       +
                     </div>
                   </div>
-
-              </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="mid-frame">
-
-            <div className="column-frame">
-              <div className="dataGridCell">
-                <div className="dataGridCell-cont">
-
-                
-
-                   
-
-                    <div className='subject' style={{ fontSize: `${fontSize}px` }}>
-                    Subject: { emailDetails.subject }
-                    </div>   
-                    
-                    <div className="EmailContentDiv">
-                        <div
-                            className="EmailContent"
-                            style={{ fontSize: `${fontSize}px` }}
-                        >
-                        { emailDetails.body }
-                        </div>
-                    </div>
-
+          <div className="homeRowFrame">
+            <div className="homeDataGridCell">
+              <div className="homeDataGridCell-child">
+                <div className="EmailContentDiv">
+                  <div
+                    className="EmailContent"
+                    style={{ fontSize: `${fontSize}px` }}
+                  >
+                    {body || "No body"}
+                  </div>
                 </div>
               </div>
             </div>
