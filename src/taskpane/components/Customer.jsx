1e6529c4b8ac2ba53ede4c296b486f1f844bf794
Vignesh <vignesh@turnkey.work>
2024-02-11 17:44:43 +0530

    testing customerResponsePopup

--- src/taskpane/components/Customer.jsx
+++ src/taskpane/components/Customer.jsx
@@ -29,7 +29,7 @@ const Customer = () => {
         <div className="row">
           <div className="datagridcell">
             <div className="container">
-              <div className="hiJohnDeoLoremIpsum">Hi John Deo,  Lorem ipsum dolor sit amet consectetur. Tortor mus rutrum sed dui malesuada risus tempor. Elit auctor eget libero et lacinia elit purus elit. Tellus egestas quam massa ac aliquam lorem. <br/>Material Cost: 120 $<br/>Lead time: 2 Days<br/>Freight Cost: 100 $  Total cost: 220$<br/><br/>Scelerisque dignissim porta libero eu auctor aliquam donec duis rhoncus. Nunc duis mattis sed dui rhoncus. Sed interdum dolor lacus a enim vel interdum odio. Lobortis eu enim viverra gravida nec in sagittis justo.<br/><br/>Thanks & Regards, ABC</div>
+              <div className="hiJohnDeoLoremIpsum">Hello John Deo,  Lorem ipsum dolor sit amet consectetur. Tortor mus rutrum sed dui malesuada risus tempor. Elit auctor eget libero et lacinia elit purus elit. Tellus egestas quam massa ac aliquam lorem. <br/>Material Cost: 120 $<br/>Lead time: 2 Days<br/>Freight Cost: 100 $  Total cost: 220$<br/><br/>Scelerisque dignissim porta libero eu auctor aliquam donec duis rhoncus. Nunc duis mattis sed dui rhoncus. Sed interdum dolor lacus a enim vel interdum odio. Lobortis eu enim viverra gravida nec in sagittis justo.<br/><br/>Thanks & Regards, ABC</div>
             </div>
           </div>
         </div>
