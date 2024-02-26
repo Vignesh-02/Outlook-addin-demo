f972e0779705e928edf5b94ec002c07b28816e35
Vignesh <vignesh@turnkey.work>
2024-02-26 11:03:40 +0530

    fixed input for customerReply

--- src/taskpane/components/Customer/CustomerResponsePopup.jsx
+++ src/taskpane/components/Customer/CustomerResponsePopup.jsx
@@ -18,7 +18,6 @@ const CustomerResponsePopup = ({  close, customerBody, setCustomerBody }) => {
     const accessToken = 'EwBwA8l6BAAUs5+HQn0N+h2FxWzLS31ZgQVuHsYAAc+bSkBFLqj+5ck8r83hgWdZHAlzfv+FntC95fUPKI4Vvt5mNZPqXHB0g0Nn8Mr6MtXSCv/1yXLBc7Q9013+d2vlY0+6M8yDAeD7mH2fCDZWY/86WYAdkIy2YMXUa0PfwQ9+X3oN2kJx2JvdlLJ8A6zFXkdK1q4d8m8UP7kynuDZ24xPWfMsyX2rhVr0eJIjg7Hz6C7aA5Z7Fcb7JjASA1si/Vq4TJj9vV984/u76k7eH7r+VWEN+Zu8QmfsbdJYAjmxX1erdZ+PskXrL42h8WCcKoQKNsZFiAPn5veZ8bLA2HJFBuq4tCgnzm6Aus4kPNz8pksjJ7Q50h7RnVuuGS4DZgAACDMt4p7XEF64QAI32Njg27hwW++SMMmM/QRjctwKtZigKFNBVdimaLLMHeNElW1WdTPIHHObL7GTiLpiv1L7G/7jDZE1ViDum1JdUTmBXsoND5JX25CsKIl2tiu/wjTZNbiouOuL7YHLxvHbBqmy2rOKnI4vRy1dCZwBUwnx9cD5aA31v5SRcx0Bp7ffbrGtyp07ba8yBaiVFXr+PRDgEHFKF1k1uuYqskLGWpZHDb9ib5ok8MOBMCT3Phi6jM3n4V27eH74XjObjmTFlxmARBMsKc03NnTNzXunN9t5NpJpRHshhkqUrJvmi07HiOtms7MM3gxqNUooTN/IRiqhXawrhob2pdCjc0bu+BtHRzT7DEyO25Zc6yuo2j1jX023bxkKnxY+mm14HAxLO7c25R7EwWFAlmN938nE+qDoT64y2OQkLkH/jmwH3nzSLTG2LicF1227vsX9q0S9DguSEx/Awj7w5tikRAL/yzFTuiw6P702KQabqB9FAaLlsGyOqeQfoosqbQxllH1jfNHtOX/nCFnJg9Ueelhrv9lOqFe2qz3W1xUD9arurezLQpjNZH+8ipVeSqmqyzhZ5cXA3XbTyvt0qogXZ0Rgjca/k7A9Rp8HDKFNxESccq5NLl7IgjUXt/X+o/JU3y4dh/afIfIGvYjIa54P3LEVYOc1JtA9C6KYvyNw52/2auxCiKFuOn6WHenfFAbAu3fqdR9xjYuNh4ogVTZeMvbQw2uokYTtCe+aO3LLUbUhRVmPsmoGUtlj9xAEIE9a24x9Ag==';
     const sendMailUrl = 'https://graph.microsoft.com/v1.0/me/messages/AQMkADAwATM0MDAAMS02OGVmLWEyN2EtMDACLTAwCgBGAAADpd4QqRN3K0yHpymIIakdnQcA54fYm3+x4Uy3lcgUc2eXoQAAAgEMAAAA54fYm3+x4Uy3lcgUc2eXoQAAABfpxN8AAAA=/reply';
         const emailData = {
-          message: {
             message: {
                 toRecipients: [
                     {
@@ -29,7 +28,6 @@ const CustomerResponsePopup = ({  close, customerBody, setCustomerBody }) => {
                 ]
             },
             comment: customerBody
-        }
     };
       
         fetch(sendMailUrl, {
